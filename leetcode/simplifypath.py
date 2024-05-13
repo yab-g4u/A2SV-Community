391b28b642d61cea29234ce677f3fbf54fb5030d
Betha <140443069+yab-g4u@users.noreply.github.com>
2024-05-13 18:11:36 +0300

    O(n)

---
+++ leetcode/simplifypath.py
@@ -0,0 +1,11 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack=[]
+        for token in path.split('/'):
+            if token =='..':
+                if stack:
+                    stack.pop()
+            elif token and token != '.':
+                stack.append(token)
+            
+        return '/' + '/'.join(stack)
