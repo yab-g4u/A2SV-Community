652bdfe2d498af788636f8600b6a872dcdd84472
Betha <140443069+yab-g4u@users.noreply.github.com>
2024-05-15 15:39:08 +0300

    stack

---
+++ leetcode/score-of-parentheses.py
@@ -0,0 +1,15 @@
+class Solution:
+    def scoreOfParentheses(self, s: str) -> int:
+        stack = []
+        score = 0
+
+        for char in s:
+            if char == '(':
+                stack.append(score)
+                score = 0
+            else:  # char == ')'
+                if not stack:
+                    return 0  # Invalid string
+                score = stack.pop() + max(2 * score, 1)
+
+        return score
