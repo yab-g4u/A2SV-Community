0b229937266c3dbbf1f550385c3a3f21c88cbf71
Betha <140443069+yab-g4u@users.noreply.github.com>
2024-05-15 16:09:23 +0300

    monotonic-stack

---
+++ leetcode/design-linked-list.py
@@ -0,0 +1,57 @@
+class Node:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class MyLinkedList:
+
+    def __init__(self):
+        self.head = None
+        self.size = 0
+
+    def get(self, index: int) -> int:
+        if index < 0 or index >= self.size:
+            return -1
+        current = self.head
+        for _ in range(index):
+            current = current.next
+        return current.val
+
+    def addAtHead(self, val: int) -> None:
+        self.head = Node(val, self.head)
+        self.size += 1
+
+    def addAtTail(self, val: int) -> None:
+        if not self.head:
+            self.addAtHead(val)
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = Node(val)
+        self.size += 1
+
+    def addAtIndex(self, index: int, val: int) -> None:
+        if index < 0 or index > self.size:
+            return
+        if index == 0:
+            self.addAtHead(val)
+            return
+        current = self.head
+        for _ in range(index - 1):
+            current = current.next
+        current.next = Node(val, current.next)
+        self.size += 1
+
+    def deleteAtIndex(self, index: int) -> None:
+        if index < 0 or index >= self.size:
+            return
+        if index == 0:
+            self.head = self.head.next
+            self.size -= 1
+            return
+        current = self.head
+        for _ in range(index - 1):
+            current = current.next
+        current.next = current.next.next
+        self.size -= 1
